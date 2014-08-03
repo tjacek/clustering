df6912cf8fa473fa6214e8d9bc98adc43697c99b
tjacek <jacekt89@gmail.com>
2014-08-03 13:49:25 -0400

    Obliczenie wartosci i wektorow wlasnych

--- src/pca.py
+++ src/pca.py
@@ -4,9 +4,27 @@ def pca(points):
     data=np.asmatrix(points)
     return pca_algorithm(data)
 
+class PcaConverter(object):
+    def __init__(self,eigen):
+        self.sort(eigen[0],eigen[1])
+
+    def sort(self,eigenvalues,eigenvectors):
+        print(eigenvalues)
+        print(eigenvectors)
+	indexes={}
+        for i,eigen in enumerate(eigenvalues):
+	    indexes[eigen]=i
+	self.eigenvalues=eigenvalues
+        self.eigenvalues.sort()
+        self.eigenvectors=np.zeros(eigenvectors.shape)
+        for i,eigen in enumerate(eigenvalues):
+            j=indexes[eigen]
+	    self.eigenvectors[i]=eigenvectors[j]
+
 def pca_algorithm(data):
     cov_matrix=get_cov_matrix(data)
-    return cov_matrix
+    eigenvalues=np.linalg.eig(cov_matrix)
+    return PcaConverter(eigenvalues)
 
 def get_cov_matrix(data):
     u=np.apply_along_axis(avg, axis=1, arr=data)
@@ -22,10 +40,12 @@ def avg(array):
     return total_sum/n
 
 def pca_test():
-    points=[[1.0,2.0,3.0,4.0],
-            [2.0,3.0,4.0,5.0],
-            [3.0,4.0,5.0,6.0]]
+    points=[[0.0,0.0,0.0,1.0],
+            [0.0,0.0,2.0,0.0],
+            [0.0,3.0,0.0,0.0],
+            [4.0,0.0,0.0,0.0]]
     result=pca(points)
-    print(result)
+    print(result.eigenvalues)
+    print(result.eigenvectors)
 
 pca_test()
