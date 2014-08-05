b312ec86146c7c61e92eafbfd0f2262ef684b6ea
tjacek <jacekt89@gmail.com>
2014-08-05 07:34:41 -0400

    Pelna implementacja algorytmu kmeans

--- src/kmeans.py
+++ src/kmeans.py
@@ -1,7 +1,15 @@
-import math,numpy as np
+import math,random,numpy as np
+import cluster_generator
 
-def kmeans(points,k=2,dim=2):
+def kmeans(points,k=2,dim=2,maxIterations=1000):
     clusters=[Cluster(dim) for i in range(k)]
+    assig=init_assigment(points,clusters)
+    assig_changed=True
+    iterations=0
+    while(assig_changed and iterations<maxIterations):
+    	assig_changed=new_assigment(points,clusters,assig)
+        iterations+=1
+    print(iterations)
     return clusters
 
 class Cluster(object):
@@ -10,14 +18,54 @@ class Cluster(object):
         self.points=[] #Punkty maja typ array
         self.means=np.zeros(dim)
 
-    def means(self):
+    def size(self):
+        return float(len(self.points))
+
+    def add_point(self,point):
+        self.points.append(point)
+
+    def reset(self):
+        self.points=[]
+
+    def compute_mean(self):
         for i in range(self.dim):
 	    get_cord=lambda x:x[i]
             dim_i=map(get_cord,self.points)
+            if(self.size()==0.0):
+               break
             self.means[i]=sum(dim_i)/self.size()
+        return self.means
 
-    def size():
-        return float(len(self.points))
+def init_assigment(points,clusters):
+    k=len(clusters)
+    assigment=np.zeros(len(points))
+    for i,point in enumerate(points):
+        cls=random.randrange(k)
+        clusters[cls].add_point(point)
+        assigment[i]=cls
+    return assigment
+
+def new_assigment(points,clusters,old_assigment):
+    assigment_changed=False
+    compute_means(clusters)
+    for i,point in enumerate(points):
+        cls_index=next_cluster(point,clusters)
+        if(old_assigment[i]!=cls_index):
+            assigment_changed=True
+            old_assigment[i]=cls_index
+            clusters[cls_index].add_point(point)
+    return assigment_changed
+
+def compute_means(clusters):
+    for cluster in clusters:
+        cluster.compute_mean()
+        cluster.reset()
+    return clusters
+
+def next_cluster(point,clusters):
+    distance=lambda cls:euclidean_metric(point,cls.means)
+    distances = [distance(cls) for cls in clusters]
+    return distances.index(min(distances))
 
 def euclidean_metric(x,y):
     value=0.0
@@ -26,5 +74,5 @@ def euclidean_metric(x,y):
     return math.sqrt(value)
 
 if __name__ == "__main__":
-    print(euclidean_metric([1.0,1.0,1.0],[2.0,2.0,2.0]))
-    kmeans([])
+    points=cluster_generator.four_clusters()
+    kmeans(points)
