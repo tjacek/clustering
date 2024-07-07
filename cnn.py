1f5ed9fc06b8b23744ddff7a509fa7ac0abbfcdb
tjacek <jacekt89@gmail.com>
2024-07-07 23:56:54 +0200

    cnn params

--- cnn.py
+++ cnn.py
@@ -1,15 +1,30 @@
 def make_cnn(params):
 	model = Sequential()
 
-	model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', strides=1, padding='same', data_format='channels_last',
-                 input_shape=(28,28,1)))
+	model.add(Conv2D(filters=params['n_kern1'], 
+		             kernel_size=params['kern_size1'], 
+		             activation='relu', strides=1, 
+		             padding='same', 
+		             data_format='channels_last',
+                      input_shape=(28,28,1)))
 	model.add(BatchNormalization())
-	model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', strides=1, padding='same', data_format='channels_last'))
+	model.add(Conv2D(filters=params['n_kern2'], 
+		             kernel_size=params['kern_size2'],
+		             activation='relu', 
+		             strides=1, padding='same', 
+		             data_format='channels_last'))
 	model.add(BatchNormalization())
-	model.add(MaxPooling2D(pool_size=(2, 2), strides=2, padding='valid' ))
+	model.add(MaxPooling2D(pool_size=(2, 2), 
+		                   strides=2, 
+		                   padding='valid' ))
 	model.add(Dropout(0.25))
 
-	model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', strides=1, padding='same', data_format='channels_last'))
+	model.add(Conv2D(filters=params['n_kern3'], 
+		             kernel_size=params['kern_size3'],
+		             activation='relu', 
+		             strides=1, 
+		             padding='same', 
+		             data_format='channels_last'))
 	model.add(BatchNormalization())
 	model.add(Conv2D(filters=64, kernel_size=(3, 3), strides=1, padding='same', activation='relu', data_format='channels_last'))
 	model.add(BatchNormalization())
@@ -27,4 +42,7 @@ def make_cnn(params):
 	return model
 
 def default_params():
-    return {"n_cats":10}
+    return {'n_kern1':32, "kern_size1":(3,3),
+            'n_kern2':32, "kern_size2":(3,3),
+            'n_kern3':64, "kern_size3":(3,3),  
+            "n_cats":10}
