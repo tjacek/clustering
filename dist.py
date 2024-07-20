3639cba9669b5c5f27f0811f5233d943d3171928
tjacek <tjacek@agh.edu.pl>
2024-07-21 01:04:37 +0200

    plot density

--- dist.py
+++ dist.py
@@ -15,20 +15,31 @@ def centroid_distance(dataset):
 
 def k_density(distances):
     kernel_alg=KernelDensity(kernel="gaussian", 
-                    bandwidth=0.5)
-    X_plot = np.linspace(0, 200, 1000)[:, np.newaxis]
+                    bandwidth=1.0)#0.5)
+    X_plot = np.linspace(0, 140, 1000)[:, np.newaxis]
     all_dens=[]
     for distance_i in distances:    
         X=np.array(distance_i).reshape(-1, 1)
         kde = kernel_alg.fit(X)
         log_dens = kde.score_samples(X_plot)
         all_dens.append(log_dens)
-    return all_dens
+    return all_dens,X_plot
+
+def show_plot(all_dens,X_plot):
+    fig, ax = plt.subplots()
+    for dens_i in all_dens:
+        ax.plot(X_plot[:, 0],
+                np.exp(dens_i),
+#    linestyle="-",
+#    label="0"
+        )
+    plt.show()
 
 exp=base.read_exp("simple_cnn")
-feat=exp.get_features('dense_1')
+feat=exp.get_features('dense')
 distances,centroids=centroid_distance(feat)
-k_density(distances)
+all_dens,X_plot=k_density(distances)
+show_plot(all_dens,X_plot)
 
 #fig, ax = plt.subplots()
 #kde = KernelDensity(kernel="gaussian", 
