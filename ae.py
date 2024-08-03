cd3058df79c2a320ffa48c76975f892bf2f574a3
tjacek <tjacek@agh.edu.pl>
2024-08-03 23:41:58 +0200

    ae params

--- ae.py
+++ ae.py
@@ -6,28 +6,29 @@ import base
 
 def make_ae(params):
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), 
+    model.add(Conv2D( params['n_kern1'], 
+                      params['kern_size1'], 
                       activation="relu", 
                       padding="same",
                       input_shape=(28,28,1)))
-    model.add(MaxPooling2D((2, 2), 
+    model.add(MaxPooling2D(params['max_pool1'], 
                            padding="same"))
-    model.add(Conv2D(32, 
-                     (3, 3), 
+    model.add(Conv2D( params['n_kern2'], 
+                      params['kern_size2'], 
                      activation="relu", 
                      padding="same"))
-    model.add(MaxPooling2D((2, 2), 
+    model.add(MaxPooling2D(params['max_pool2'], 
                            padding="same"))
     model.add(Flatten())  
     model.add(Dense(1568, activation='relu'))
     model.add(Reshape(target_shape=(7, 7, 32)))
-    model.add(Conv2DTranspose(32, 
-                             (3, 3), 
+    model.add(Conv2DTranspose(params['n_kern2'], 
+                              params['kern_size2'], 
                              strides=2, 
                              activation="relu", 
                              padding="same"))
-    model.add(Conv2DTranspose(32, 
-                             (3, 3), 
+    model.add(Conv2DTranspose(params['n_kern1'], 
+                              params['kern_size1'], 
                              strides=2, 
                              activation="relu", 
                              padding="same"))
@@ -37,9 +38,16 @@ def make_ae(params):
                     padding="same"))
     return model
 
+
+def default_params():
+    return {'n_kern1':32, "kern_size1":(3,3),
+            'n_kern2':32, "kern_size2":(3,3),
+            'max_pool1':(2,2),'max_pool2':(2,2) }
+
 def simple_exp(epochs=2,batch_size = 64,out_path=None):
     data=base.get_minst_dataset()
-    autoencoder=make_ae(params=None)
+    params=default_params()
+    autoencoder=make_ae(params=params)
     autoencoder.compile(optimizer="adam", 
                         loss="binary_crossentropy")
     autoencoder.summary()
