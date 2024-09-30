f37598423c7bc68aac950b69f5994bee44f6fc3c
tjacek <jacekt89@gmail.com>
2024-09-30 23:21:03 +0200

    Points class

--- tab.py
+++ tab.py
@@ -4,21 +4,41 @@ from sklearn import preprocessing
 import gudhi as gd
 import matplotlib.pyplot as plt
 
+class Points(object):
+    def __init__(self,X,y):
+        self.X=X
+        self.y=y
+
+    def n_cats(self):
+        return max(self.y)+1
+    
+    def select(self,cat_i:int):
+        return self.X[self.y==cat_i]
+
+    def get_diag(self,cat_i=None):
+        if(cat_i is None):
+            points=self.X
+        else:
+            points=self.select(cat_i)
+        rips_complex = gd.RipsComplex(points=points, 
+                                      max_edge_length=2.0)
+        simplex_tree = rips_complex.create_simplex_tree(max_dimension=13)
+        return simplex_tree.persistence(min_persistence=0.4)
+
 def read_csv(in_path:str):
     df=pd.read_csv(in_path)
     raw=df.to_numpy()
     X,y=raw[:,:-1],raw[:,-1]
     X= preprocessing.RobustScaler().fit_transform(X)
     print(X.shape)
-    return X,y
-#    return Dataset(X,y)
+    return Points(X,y)
 
-X,y=read_csv("../uci/cleveland")
 
-rips_complex = gd.RipsComplex(points=X, 
-                              max_edge_length=2.0)
-simplex_tree = rips_complex.create_simplex_tree(max_dimension=13)
-diag=simplex_tree.persistence(min_persistence=0.4)
+points=read_csv("../uci/cleveland")
+print(points.n_cats())
+print(points.select(3).shape)
+
+diag=points.get_diag(4)
 gd.plot_persistence_barcode(diag)
 #ax = gd.plot_persistence_diagram(diag)
 #ax.set_aspect("equal")  # 
