c85ff265711f732d0d9680c128c8bece23092c3c
tjacek <jacekt89@gmail.com>
2026-03-04 23:49:13 +0100

    eval clusters

--- kmeans.py
+++ kmeans.py
@@ -3,6 +3,7 @@ from sklearn.metrics import silhouette_samples,silhouette_score
 import cnn
 
 def cluster(n_clusters=3):
+    cnn
     model,data=cnn.simple_exp()
     feat=model.extract(data.train)
     kmeans = KMeans(n_clusters=n_clusters, 
@@ -11,8 +12,15 @@ def cluster(n_clusters=3):
     return kmeans.labels_,data,feat
 
 
-clust,data,feat=cluster(n_clusters=3)
-silhouette_avg = silhouette_score(feat, clust)
-sample_silhouette_values = silhouette_samples(feat, 
+def eval_cluster(n_clusters=3):
+    clust,data,feat=cluster(n_clusters)
+    silhouette_avg = silhouette_score(feat, clust)
+    sample_silhouette_values = silhouette_samples(feat, 
 	                                          clust)
-print(silhouette_avg)
+    return silhouette_avg
+
+values=[3,5,7]
+for n_clusters in values:
+    avg_i=eval_cluster(n_clusters)
+    print(n_clusters)
+    print(avg_i)
