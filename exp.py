2a8e7ec91c24b0a557cc9aeaf81aa2eb978d24dd
tjacek <jacekt89@gmail.com>
2026-05-04 23:32:41 +0200

    train model

--- exp.py
+++ exp.py
@@ -2,19 +2,41 @@ import base
 from dataclasses import dataclass
 import ae,cnn
 
-@dataclass(frozen=True)
+@dataclass()#frozen=True)
 class Params:
     model:str = "cnn"
     clustering:str = "kmeans"
-
+    dataset= None
+    
     def make_model(self):
-    	if(self.model=="cnn"):
-    		return cnn.make_cnn()
-       if(self.model=="ae"):
-    		return ae.make_ae()
+        data=self.get_data().train
+        if(self.model=="cnn"):
+            model=cnn.make_cnn()
+#            model.fit(data.X,
+#                      data.y)
+        if(self.model=="ae"):
+            model=ae.make_ae()
+        model.fit(data)
+        return model
+
+    def get_data(self):
+        if(self.dataset is None):
+            self.dataset=base.get_minst_dataset()
+        return self.dataset
+
+#    def make_model(self):
+#        if(self.model=="cnn"):
+#    		   model=cnn.make_cnn()
+#            model.fit(dataset.X,
+#                      dataset.y)
+#        if(self.model=="ae"):
+#    	      model=ae.make_ae()
+#            model.fit(dataset.X)
+#        return model
 
 def simple_exp(*args):
-	params=base.Params(*args)
-	print(params)
+   params=Params(*args)
+   params.make_model()
+   print(params)
 
 simple_exp( "cnn", "kmeans")
