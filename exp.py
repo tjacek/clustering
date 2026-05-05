23f0b093368f0f25f83ac1ebd05f233fe10eb25e
tjacek <jacekt89@gmail.com>
2026-05-05 10:08:06 +0200

    refactore params

--- exp.py
+++ exp.py
@@ -2,41 +2,28 @@ import base
 from dataclasses import dataclass
 import ae,cnn
 
-@dataclass()#frozen=True)
+@dataclass(frozen=True)
 class Params:
     model:str = "cnn"
     clustering:str = "kmeans"
-    dataset= None
     
     def make_model(self):
-        data=self.get_data().train
         if(self.model=="cnn"):
             model=cnn.make_cnn()
-#            model.fit(data.X,
-#                      data.y)
         if(self.model=="ae"):
             model=ae.make_ae()
-        model.fit(data)
         return model
 
-    def get_data(self):
-        if(self.dataset is None):
-            self.dataset=base.get_minst_dataset()
-        return self.dataset
+#    def clust_alg(self):
+#        if(self.clustering=="kmeans"):
 
-#    def make_model(self):
-#        if(self.model=="cnn"):
-#    		   model=cnn.make_cnn()
-#            model.fit(dataset.X,
-#                      dataset.y)
-#        if(self.model=="ae"):
-#    	      model=ae.make_ae()
-#            model.fit(dataset.X)
-#        return model
 
 def simple_exp(*args):
-   params=Params(*args)
-   params.make_model()
-   print(params)
+    data=base.get_minst_dataset()
+    params=Params(*args)
+    model=params.make_model()
+    model.fit(data.train)
+    feat=model.extract(data.train)
+    print(params)
 
 simple_exp( "cnn", "kmeans")
