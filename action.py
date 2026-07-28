b907c9e4dbc9713962c588c5190971f36d76b611
tjacek <jacekt89@gmail.com>
2026-07-28 13:45:17 +0200

    ActionDesc

--- action.py
+++ action.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from dataclasses import dataclass
 import utils
 
 class ActionGroup(object):
@@ -9,6 +10,9 @@ class ActionGroup(object):
     def __len__(self):
         return len(self.actions)
     
+    def __getitem__(self,i):
+        return self.actions[i]
+
     @classmethod
     def read(cls,in_path):
         actions=[ Action.read(path_i) 
@@ -18,18 +22,40 @@ class ActionGroup(object):
     def  max_dims(self):
          return [action_i.max_dims() for action_i in self.actions] 
 
+
+@dataclass(frozen=True)
+class ActionDesc:
+    name:str
+    cat:int
+    person:int  
+    
+    @classmethod
+    def from_path(cls,path):
+        name=path.split("/")[-1]
+        raw=name.split("_")
+        return cls(name=name,
+                   cat=int(raw[0]), 
+                   person=int(raw[1]))
+
 class Action(object):
-    def __init__(self,frames):
+    def __init__( self,
+                 frames,
+                 desc):
         self.frames=frames
+        self.desc=desc
     
     def __len__(self):
     	return len(self.frames)
+
+    def __str__(self):
+        return self.desc.name
     
     @classmethod
     def read(cls,in_path):
         frames=[cv2.imread(path_i,cv2.IMREAD_GRAYSCALE)
                     for path_i in utils.top_files(in_path)]
-        return cls(frames)
+        desc=ActionDesc.from_path(in_path)
+        return cls(frames,desc)
 
     def shape(self):
     	return [frame_i.shape for frame_i in self.frames]
@@ -40,4 +66,4 @@ class Action(object):
     	return np.amax(arr,axis=0)
 
 actions=ActionGroup.read("MSR")
-print(actions.max_dims())
+print(f"{actions[0]}")
