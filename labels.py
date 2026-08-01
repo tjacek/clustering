0a4505e5df3e728d6b49c90623d35353b8779135
tjacek <jacekt89@gmail.com>
2026-08-01 16:57:56 +0200

    by labels

---
+++ labels.py
@@ -0,0 +1,93 @@
+import numpy as np
+import string
+from collections import defaultdict
+import seq,cnn,utils
+
+class LabelingGroup(seq.SeqGroup):
+    @classmethod
+    def dtype(cls):
+        return Labeling
+
+    @classmethod
+    def from_actions(cls, action_path, model_path):
+        return cls._from_actions(action_path, model_path,
+                                  lambda model, X: model.predict(X))
+    
+    def by_labels(self, seq_group):
+        frame_dict=defaultdict(lambda :[])
+        label_dict=self.as_dict()
+        for seq_i in seq_group:
+            labeling_i=label_dict[seq_i.desc.name]
+            for label_j,frame_j in zip(labeling_i,seq_i):
+                frame_dict[int(label_j)].append(frame_j)
+        
+        dtype=seq_group.dtype()
+        def helper(i):
+            return seq.ActionDesc(f"label_{i}",
+                                  None,
+                                  None)
+        frame_dict={ i:dtype(frames_i,helper(i))
+                        for i,frames_i in frame_dict.items()}
+        return frame_dict
+
+    def unique(self):
+        labels=[]
+        for seq_i in self:
+            labels+=seq_i.unique()
+        labels=list(set(labels))
+        return np.array(labels)
+
+class Labeling(seq.Seq):
+    @classmethod
+    def read(cls,in_path):
+        arr=np.loadtxt(in_path)
+        desc=seq.ActionDesc.from_path(in_path)
+        return cls(arr,desc)
+
+    def save(self,out_path):
+        np.savetxt(out_path,self)
+    
+    def unique(self):
+        return list(set(self))
+
+    def as_symbols(self):
+        letters = list(string.ascii_lowercase)
+        n=len(letters)
+        symb_seq=""
+        def helper(i):
+            symb=letters[i%n]
+            k= int(np.floor(i/n))
+            return symb+str(k)
+        for i in self:
+            symb_seq+=helper(i)
+        return symb_seq
+
+def train( in_path,
+           out_path,
+           epochs=150):
+    actions=seq.ActionGroup.read(in_path)
+    train,test=actions.split()
+    model,_=cnn.cnn_exp( train.as_dataset(),
+                         test.as_dataset(),
+                         cnn.frame_params(),
+                         epochs=epochs)
+    model.save(out_path)
+
+def save_by_labels( label_path,
+                    action_path,
+                    out_path):
+    labels=LabelingGroup.read(label_path)
+    actions=seq.ActionGroup.read(action_path)
+    label_dict=labels.by_labels(actions)
+    utils.make_dir(out_path)
+    items=label_dict.items()
+    for i,(cat_i,group_i) in enumerate(items):
+        print(cat_i)
+        group_i.save(f"{out_path}/{i}")
+#train("MSR/scaled","MSR/model")
+#seqs=FeatSeqGroup.from_actions("MSR/scaled","MSR/model.keras")
+#labels=LabelingGroup.from_actions("MSR/scaled","MSR/model.keras")
+#labels.save("MSR/labels")
+save_by_labels( "MSR/labels",
+                "MSR/scaled",
+                "MSR/by_labels")
--- seq.py
+++ seq.py
@@ -1,8 +1,154 @@
 import numpy as np
-import string
-import action,cnn,utils
+import cv2
+from collections import defaultdict
+from dataclasses import dataclass
+import base,cnn,utils
 
-class FeatSeqGroup(action.SeqGroup):
+class SeqGroup(list):
+    def __init__(self, actions=None):
+        if(actions is None):
+            actions=[]
+        super().__init__(actions)
+    
+    @classmethod
+    def dtype(cls):
+        raise NotImplementedError
+
+    def map(self,fun):
+        seqs=[seq_i.map(fun) 
+                   for seq_i in self]
+        return self.__class__(seqs)
+
+    @classmethod
+    def read(cls,in_path):
+        dtype=cls.dtype()
+        seqs=[  dtype.read(path_i) 
+                    for path_i in utils.top_files(in_path)]
+        return cls(seqs)
+
+    def save(self,out_path):
+        utils.make_dir(out_path)
+        for seq_i in self:
+            seq_i.save(f"{out_path}/{seq_i}")
+   
+    def flatten(self,fun):
+        all_items=[]
+        for action_i in self:
+            all_items.extend(action_i.eval(fun))
+        return all_items
+
+    def split(self,fun=None):
+        if(fun is None):
+            fun= lambda desc: (desc.person % 2)==1
+        train,test=self.__class__([]), self.__class__([])
+        for action_i in self:
+            if(fun(action_i.desc)):
+                train.append(action_i)
+            else:
+                test.append(action_i)
+        return train,test
+    
+    def as_dataset(self):
+        X,y=[],[]
+        for action_i in self:
+            for frame_j in action_i:
+                X.append(frame_j)
+                y.append(action_i.desc.cat)
+        return base.Dataset(X=np.array(X),
+                            y=np.array(y))
+    
+    def as_dict(self):
+        return { seq_i.desc.name:seq_i
+                    for seq_i in self}
+
+    @classmethod
+    def _from_actions(cls, action_path, model_path, fun):
+        actions = ActionGroup.read(action_path)
+        model = cnn.ConvNN.read(model_path)
+        seqs = cls([])
+        for action_i in actions:
+            X = np.array(action_i)
+            seq_i = cls.dtype()(frames=fun(model, X), desc=action_i.desc)
+            seqs.append(seq_i)
+        return seqs
+
+    def by_cat(self):
+        cat_dict = defaultdict(lambda: self.__class__())
+        for seq_i in self:
+            cat_i=seq_i.desc.cat
+            cat_dict[cat_i].append(seq_i)
+        return cat_dict
+
+class Seq(list):
+    def __init__( self, 
+                  frames,
+                  desc):
+        super().__init__(frames)
+        self.desc=desc
+    
+    def __str__(self):
+        return self.desc.name
+    
+    def eval(self,fun):
+        return [ fun(frame_i) for frame_i in self]
+    
+    def map(self,fun):
+        return self.__class__( frames=self.eval(fun),
+                               desc=self.desc )
+@dataclass(frozen=True)
+class ActionDesc:
+    name:str
+    cat:int
+    person:int  
+    
+    @classmethod
+    def from_path(cls,path):
+        name=path.split("/")[-1]
+        raw=name.split("_")
+        return cls(name=name,
+                   cat=int(raw[0])-1, 
+                   person=int(raw[1]))
+
+class ActionGroup(SeqGroup):
+    @classmethod
+    def dtype(cls):
+        return Action
+
+    def stats(self,fun):
+        values=self.flatten(fun)
+        print(f"Mean:{np.mean(values)}")
+        print(f"Median:{np.median(values)}")
+        print(f"Max:{np.amax(values)}")
+        print(f"Min:{np.amin(values)}")
+
+    def rescale( self, 
+                 new_width=80,
+                 new_height=240):
+        def helper(frame):
+            return cv2.resize(frame, (new_width, new_height))
+        return self.map(helper)
+
+class Action(Seq):
+
+    @classmethod
+    def read(cls,in_path):
+        frames=[cv2.imread(path_i,cv2.IMREAD_GRAYSCALE)
+                    for path_i in utils.top_files(in_path)]
+        desc=ActionDesc.from_path(in_path)
+        return cls(frames,desc)
+
+    def save(self,out_path):
+        utils.make_dir(out_path)
+        for i,frame_i in enumerate(self):
+            cv2.imwrite(f"{out_path}/{i}.png", frame_i)
+
+def height(frame):
+    return frame.shape[0]
+
+def width(frame):
+    return frame.shape[1]
+
+class FeatSeqGroup(SeqGroup):
     @classmethod
     def dtype(cls):
         return FeatSeq
@@ -12,62 +158,10 @@ class FeatSeqGroup(action.SeqGroup):
         return cls._from_actions(action_path, model_path,
                                   lambda model, X: model.extract(X, n_layer))
 
-class FeatSeq(action.Seq):
+class FeatSeq(Seq):
     def save(self,out_path):
-        np.savetxt(out_path,self)
+        np.save(out_path,self)
 
-class LabelingGroup(action.SeqGroup):
-    @classmethod
-    def dtype(cls):
-        return Labeling
+if __name__ == '__main__':
+    actions=ActionGroup.read("MSR(scaled)")
 
-    @classmethod
-    def from_actions(cls, action_path, model_path):
-        return cls._from_actions(action_path, model_path,
-                                  lambda model, X: model.predict(X))
-
-    def unique(self):
-        labels=[]
-        for seq_i in self:
-            labels+=seq_i.unique()
-        labels=list(set(labels))
-        return np.array(labels)
-
-class Labeling(action.Seq):
-    def save(self,out_path):
-        np.savetxt(out_path,self)
-    
-    def unique(self):
-        return list(set(self))
-
-    def as_symbols(self):
-        letters = list(string.ascii_lowercase)
-        n=len(letters)
-        symb_seq=""
-        def helper(i):
-            symb=letters[i%n]
-            k= int(np.floor(i/n))
-            return symb+str(k)
-        for i in self:
-            symb_seq+=helper(i)
-        return symb_seq
-
-def train( in_path,
-           out_path,
-           epochs=150):
-    actions=action.ActionGroup.read(in_path)
-    train,test=actions.split()
-    model,_=cnn.cnn_exp( train.as_dataset(),
-                         test.as_dataset(),
-                         cnn.frame_params(),
-                         epochs=epochs)
-    model.save(out_path)
-
-#train("MSR/scaled","MSR/model")
-#seqs=FeatSeqGroup.from_actions("MSR/scaled","MSR/model.keras")
-seqs=LabelingGroup.from_actions("MSR/scaled","MSR/model.keras")
-cat_dict=seqs.by_cat()
-for cat_i,group_i in cat_dict.items():
-    print(cat_i)
-    print(group_i.unique())
-#seqs.save("MSR/seqs")
