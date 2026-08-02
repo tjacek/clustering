bb2ecf6b408540d80a422b7a9c31eb6306898d13
tjacek <jacekt89@gmail.com>
2026-08-02 23:36:52 +0200

    ClusterAssig

--- clust_algs.py
+++ clust_algs.py
@@ -4,23 +4,32 @@ from sklearn.cluster import SpectralClustering
 from sklearn.metrics import silhouette_samples,silhouette_score
 from tqdm import tqdm
 import labels
-#class C
+
+class ClusterAsig(object):
+	def __init__( self,
+		          preclustr,
+		          labels):
+		self.preclustr=preclustr
+		self.labels=labels
+
+	def score(self):
+		return silhouette_score( self.preclustr.frames,
+	                             self.labels,
+	                             metric='euclidean')
 
 def get_cluster_alg(alg_type):
     if(alg_type=="spectral"):
         return spectral_alg
     return kmeans_alg
 
-def kmeans_alg(frames,
+def kmeans_alg(preclustr,
                n_clusters=2):
     kmeans = KMeans(n_clusters=n_clusters, 
 	                random_state=0, 
-	                n_init="auto").fit(frames)
-    return kmeans.labels_ 
-#    assig=ClusterAsig( labels=kmeans.labels_,
-#                       feat=feat)
-#    clust=KMeansClust(centroids=kmeans.cluster_centers_)
-#    return clust,assig
+	                n_init="auto")
+    kmeans.fit(preclustr.frames)
+    return ClusterAsig(  preclustr=preclustr,
+    	                 labels=kmeans.labels_)
 
 def spectral_alg(data,
                feat,
@@ -39,17 +48,20 @@ def find_number( precluster,
 	             max_cluster=50):
     alg=get_cluster_alg(alg_type)
     all_scores=[]
-    for k in tqdm(range(max_cluster)):
-        assig_i=alg(precluster.frames,k+2)
-        score_i=silhouette_score(precluster.frames,
-	                           assig_i,
-	                           metric='euclidean')
-        all_scores.append(score_i)
-    all_scores=np.array(all_scores)
-    print(all_scores)
-    x=np.array(range(max_cluster))+2
-    plt.scatter(x, all_scores,  alpha=0.5)
+    sizes=np.array(range(max_cluster))+2
+    assig=[ alg(precluster,k) 
+              for k in tqdm(sizes)]
+    scores=np.array([assig_i.score() 
+    	        for assig_i in tqdm(assig)])
+    print(scores)
+    print(np.argmax(scores))
+    plt.scatter(sizes,scores,alpha=0.5)
+    plt.xlabel("n_clusters")
+    plt.ylabel("Silhouette")
+    plt.title(alg_type)
+    plt.grid(alpha=0.7)
     plt.show()
+
 
 seqs= labels.FeatSeqGroup.read("MSR/seq")
 precluster=seqs.as_precluster()
