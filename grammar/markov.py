3739c54b0841503c8bc87392dc31d237e695b400
tjacek <jacekt89@gmail.com>
2026-08-26 00:17:44 +0200

    Dist1D

--- grammar/markov.py
+++ grammar/markov.py
@@ -12,9 +12,16 @@ class MarkovChain(object):
         self._states=None
         self._terms=None
     
+
+    def __getitem__(self,key):
+        if(not (key in self.dist)):
+            p=1/len(self.states)
+            self.dist[key]=UniformDist1D(p)
+        return self.dist[key]
+
     @property
     def states(self):
-        if(self._terms is None):
+        if(self._states is None):
             states = list(self.dist.keys())
             states.sort()
             self._states=utils.sort(states)
@@ -30,45 +37,87 @@ class MarkovChain(object):
             self._terms=utils.sort(terms)      
         return self._terms
 
-    def as_matrix(self,irred=False):
+    def as_matrix( self,
+                   states=None,
+                   terms=None,
+                   irred=False):
+        if(states is None):
+            states=self.states
+        if(terms is None):
+            terms=self.terms
         default= self.EPS if(irred) else 0
+        if(irred):
+            Dist1D.DEFAULT=self.EPS
         matrix=[]
-        for state_i in self.states:
-            dict_i=self.dist[state_i]
+        for state_i in states:
+            dict_i=self[state_i]
             row_i=[]
-            for term_i in self.terms:
-                if(term_i in dict_i):
-                	row_i.append(dict_i[term_i])
-                else:
-                	row_i.append(default)
+            for term_i in terms:
+                row_i.append(dict_i[term_i])
             row_i=np.array(row_i)
             if(irred):
                 row_i/=np.sum(row_i)
             matrix.append(row_i)
         return np.array(matrix)
     
-    def stationary_dist(self):
-        trans=self.as_matrix(irred=True)
+    def stationary_dist( self,
+                         states=None,
+                         terms=None):
+        trans=self.as_matrix( states=states,
+                              terms=terms,
+                              irred=True)
         eig_values,eig_vectors=np.linalg.eig(trans.T)
         ones_index=np.argmin(np.abs(eig_values - 1.0))
         raw_vector=eig_vectors[:, ones_index].real
         stationary_dist= raw_vector / np.sum(raw_vector)
         return stationary_dist
-
+    
     @classmethod
     def make(cls,symbols,k=1):
-        fun=lambda :defaultdict(lambda :0)
-        ngram_dict=defaultdict(fun)
+        ngram_dict=defaultdict(lambda:Dist1D())
         for seq_i in symbols:
             ngrams_i=seq_i.ngrams(k+1)
             for ngram_j in ngrams_i:
-                start_j=ngram_j[:-1]
-                start_j=symbols.SEP.join(start_j)
-                end_j=ngram_j[-1]
-                ngram_dict[start_j][end_j]+=1
-        dist={}
-        for state_i,dict_i in ngram_dict.items():
-            norm_const=sum(list(dict_i.values()))
-            dist[state_i]={ ngram_j:(value_j/norm_const) 
-                                for ngram_j,value_j in dict_i.items()}
-        return cls(dist,k)
+                ngrams_i=seq_i.ngrams(k+1)
+                for ngram_j in ngrams_i:
+                    start_j=ngram_j[:-1]
+                    start_j=symbols.SEP.join(start_j)
+                    end_j=ngram_j[-1]
+                    ngram_dict[start_j].add(end_j)
+        ngram_dict={ state_i:dist_i.norm()
+                for state_i,dist_i in ngram_dict.items()}
+        return cls(ngram_dict,order=k)
+
+class Dist1D(object):
+    DEFAULT=0
+    def __init__( self,
+                  dist_dict=None):
+        if( dist_dict is None):
+            dist_dict={}
+        self.dist_dict=dist_dict
+    
+    def add(self,item):
+        if(item in self.dist_dict):
+            self.dist_dict[item]+=1
+        else:
+            self.dist_dict[item]=1
+        return self
+
+    def __getitem__(self,item):
+        if(item in self.dist_dict):
+            return self.dist_dict[item]
+        return self.DEFAULT
+
+    def norm(self):
+        norm_c=sum(self.dist_dict.values())
+        pairs=self.dist_dict.items()
+        self.dist_dict={ key_i:(value_i/norm_c)
+                          for key_i,value_i in pairs}
+        return self
+
+class UniformDist1D(object):
+    def __init__(self,p):
+        self.p=p
+
+    def __getitem__(self,item):
+        return self.p
