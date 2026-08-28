02c62586a953e3790e1e5908b8003d7bf0f063cf
tjacek <jacekt89@gmail.com>
2026-08-28 13:09:17 +0200

    tsne reduction

--- snapshot.py
+++ snapshot.py
@@ -1,5 +1,8 @@
+import numpy as np
+from sklearn import manifold
 import argparse
 import seq
+import plot
 
 def cluster_stats( label_path,
 	               seq_path):
@@ -10,9 +13,20 @@ def cluster_stats( label_path,
     seqs= seq_group.read(seq_path)
     by_labels=seqs.by_labels(symbols)
     for label_i,group_i in by_labels.items():
-    	print(f"Cat:{label_i}")
-    	print(len(group_i))
-    print(type(by_labels))
+        print(f"Cat:{label_i}")
+        print(len(group_i))
+        X=np.array(group_i)
+        t_sne = manifold.TSNE( n_components=2,
+                               perplexity=min(30,len(group_i)-1),
+                               init="random",
+                               max_iter=250,
+                               random_state=0,
+                              )
+        X_tsne = t_sne.fit_transform(X)
+        plot.scatter(x=X_tsne[:,0],
+                     y=X_tsne[:,1],
+                     title=label_i)
+        print(X_tsne[:,0].shape)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
