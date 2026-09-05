315c582d843c7e88dfae463551dea3befb640e59
tjacek <jacekt89@gmail.com>
2026-09-05 23:04:28 +0200

    ClusterGui base class

--- gui.py
+++ gui.py
@@ -5,45 +5,32 @@ import snapshot
 import plot 
 import reduct
 
-class GuiApp:
-    def __init__(self,
-                 root,
-                 by_labels):
+class ClusterGui:
+    def __init__( self,
+                  root,
+                  by_labels):
         self.root = root
         self.root.title("Frame Clusters")
         self.root.geometry("600x350")
-        self.root.resizable(False, False)
+#        self.root.resizable(False, False)
         self.by_labels = by_labels
-        self.current_data = None
         self.selected_cluster = None
-        self.selected_alg = None
         self.selected_label = None
-
-        title_label = tk.Label(root, text="Clusters:", font=("Arial", 12))
-        title_label.pack(pady=10)
         
-        lists_container = tk.Frame(root)
-        lists_container.pack(pady=5)
- 
-        self.cluster_list = self.create_listbox( lists_container, 
+#       title_label = tk.Label(root, text="Clusters:", font=("Arial", 12))
+#       title_label.pack(pady=10)
+        self.lists_container = tk.Frame(root)
+        self.lists_container.pack(pady=5)
+
+        self.cluster_list = self.create_listbox( self.lists_container, 
                                                  self.by_labels.names(),
                                                  side=tk.LEFT, 
-                                                 on_select=self.set_selected_cluster
-                                                )
-        self.algs_list = self.create_listbox( lists_container, 
-                                              reduct.ALGS.keys(),
-                                              side=tk.LEFT, 
-                                              on_select=self.set_selected_alg
-                                            )
-        self.label_list = self.create_listbox( lists_container, 
+                                                 on_select=self.set_selected_cluster)
+        self.label_list = self.create_listbox( self.lists_container, 
                                                self.by_labels.info_types(),
                                                side=tk.BOTTOM,
-                                               on_select=self.set_selected_label
-                                             )
-
-        confirm_button = tk.Button(root, text="Show cluster", command=self.confirm_selection)
-        confirm_button.pack(pady=10)
-
+                                               on_select=self.set_selected_label)
+    
     def create_listbox(self, parent, items, side, on_select=None):
         frame = tk.Frame(parent)
         frame.pack(side=side, padx=10)
@@ -71,20 +58,36 @@ class GuiApp:
  
         return listbox
 
+
     def set_selected_cluster(self, event):
         selected_indices = self.cluster_list.curselection()
         if selected_indices:
             self.selected_cluster = self.cluster_list.get(selected_indices[0])
- 
-    def set_selected_alg(self, event):
-        selected_indices = self.algs_list.curselection()
-        if selected_indices:
-            self.selected_alg = self.algs_list.get(selected_indices[0])
     
     def set_selected_label(self, event):
         selected_indices = self.label_list.curselection()
         if selected_indices:
             self.selected_label = self.label_list.get(selected_indices[0]) 
+
+
+class ReductionGui(ClusterGui):
+    def __init__( self,
+                  root,
+                  by_labels):
+        super(ReductionGui, self).__init__( root,
+                                            by_labels)
+        self.selected_alg = None
+        self.algs_list = self.create_listbox( self.lists_container, 
+                                              reduct.ALGS.keys(),
+                                              side=tk.LEFT, 
+                                              on_select=self.set_selected_alg)
+        confirm_button = tk.Button(root, text="Show cluster", command=self.confirm_selection)
+        confirm_button.pack(pady=10)
+
+    def set_selected_alg(self, event):
+        selected_indices = self.algs_list.curselection()
+        if selected_indices:
+            self.selected_alg = self.algs_list.get(selected_indices[0])
     
     def confirm_selection(self):
         if not self.selected_cluster:
@@ -117,6 +120,6 @@ if __name__ == "__main__":
     by_labels=snapshot.GroupedClust.make(args.cls_path,
     	                                 args.seq_path)
     root = tk.Tk()
-    app = GuiApp( root,
-    	          by_labels)
+    app = ReductionGui( root,
+    	                by_labels)
     root.mainloop()
